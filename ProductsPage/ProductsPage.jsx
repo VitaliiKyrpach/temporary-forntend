fb1481513e3805a1a11822558aa5647975419007
VitaliiKyrpach <126057381+VitaliiKyrpach@users.noreply.github.com>
2024-01-17 10:23:15 +0200

    something

--- ProductsPage/ProductsPage.jsx
+++ ProductsPage/ProductsPage.jsx
@@ -2,20 +2,21 @@ import css from './products.module.css';
 import TitlePage from 'components/TitlePage/TitlePage';
 import { ProductsFilters } from 'components/productsFilters/ProductsFilters';
 import { ProductsList } from 'components/productsList/ProductsList';
-import { ProductsModal } from 'components/productsModal/ProductsModal';
+// import { ProductsModal } from 'components/productsModal/ProductsModal';
 import { useState } from 'react';
 import { Container } from '../../components/Container/Container';
-
+import { AddProductForm } from 'components/addProductForm/AddProductForm';
+import { BasicModalWindow } from '../../modal/basicModalWindow/BasicModalWindow';
 const ProductsPage = () => {
   const [open, setOpen] = useState(false);
-  const [modal, setModal] = useState(null)
+  const [modal, setModal] = useState(null);
   const handleClose = e => {
     setOpen(false);
   };
 
-  const handleOpen = (data) => {
+  const handleOpen = data => {
     setOpen(true);
-    setModal(data)
+    setModal(data);
   };
   return (
     <div className={css.productsPage}>
@@ -27,7 +28,9 @@ const ProductsPage = () => {
         </div>
       </Container>
       {/* <div className={css.container}></div> */}
-      <ProductsModal isOpen={open} onClose={handleClose} data={modal} />
+      <BasicModalWindow isOpen={open} onClose={handleClose} data={modal}>
+        <AddProductForm data={modal} />
+      </BasicModalWindow>
     </div>
   );
 };
--- products/productsOperations.js
+++ products/productsOperations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 // const token =
-//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YTU3YWJjZWI1ZjgyMWQ3MGM4YzQwYiIsImlhdCI6MTcwNTM0MzY3NiwiZXhwIjoxNzA1NDI2NDc2fQ.4IzIO1qXg2Gzom4iBez5K8oPhGCqXDcQSEOq_57I8OA';
+//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YTU3YWJjZWI1ZjgyMWQ3MGM4YzQwYiIsImlhdCI6MTcwNTQyNzE2MiwiZXhwIjoxNzA1NTA5OTYyfQ.Y3BN8GkeG4P4n3G9lSX53IFMPA_tvZXWxrXmnbp6F60';
 // axios.defaults.baseURL = 'https://power-pulse-backend.onrender.com/';
 // axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 // const params = {
@@ -13,10 +13,10 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 // };
 
 export const fetchProducts = createAsyncThunk(
-  'products/fetchAll',
+  'products/fetchProducts',
   async (filter, { rejectWithValue }) => {
     try {
-      const params = { ...filter, limit: 10000 };
+      const params = { ...filter, limit: 500 };
       const response = await axios.get('/products', {
         params,
       });
