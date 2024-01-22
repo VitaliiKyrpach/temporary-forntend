fec1eac441acaf6665561839d26010a65d7bee72
VitaliiKyrpach <126057381+VitaliiKyrpach@users.noreply.github.com>
2024-01-22 08:55:05 +0200

    delete

--- diaryOrg/diarySelectors.js
+++
@@ -1,5 +0,0 @@
-export const selectDiaryProducts = state => state.diary.productsInDiary;
-export const selectDiaryExercises = state => state.diary.exercisesInDiary;
-export const selectDiaryIsLoading = state => state.diary.IsLoading;
-export const selectDiaryError = state => state.diary.error;
-
